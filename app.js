52c14eded4294fef03df469baa0bcdbfa4fc1fec
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2022-07-02 22:07:02 +0300

    Make some changes in app.js and use Bulma framework in index.html

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const p2Button = document.querySelector("#p2Button");
 const resetButton = document.querySelector("#reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
-const winningScoreSelect = document.querySelector("#playto");
-const winner = document.querySelector(".winner");
-const loser = document.querySelector(".loser");
+const winningScoreSelect = document.querySelector("#playTo");
 
 let p1Score = 0;
 let p2Score = 0;
@@ -17,8 +15,10 @@ p1Button.addEventListener("click", function () {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add("winner");
-      p2Display.classList.add("loser");
+      p1Display.classList.add("has-text-success");
+      p2Display.classList.add("has-text-danger");
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
     p1Display.textContent = p1Score;
   }
@@ -29,8 +29,10 @@ p2Button.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
-      p2Display.classList.add("winner");
-      p1Display.classList.add("loser");
+      p2Display.classList.add("has-text-success");
+      p1Display.classList.add("has-text-danger");
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
     p2Display.textContent = p2Score;
   }
@@ -49,6 +51,8 @@ function reset() {
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
-  p1Display.classList.remove("winner", "loser");
-  p2Display.classList.remove("winner", "loser");
+  p1Display.classList.remove("has-text-success", "has-text-danger");
+  p2Display.classList.remove("has-text-success", "has-text-danger");
+  p1Button.disabled = false;
+  p2Button.disabled = false;
 }
